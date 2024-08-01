240839fe993997ad367288468ad7c859e1212ce4
diptanshumahish <diptanshumahish2016@gmail.com>
2024-08-01 17:00:19 +0530

    version change

--- lib/constants/text.dart
+++ lib/constants/text.dart
@@ -1,4 +1,4 @@
 abstract class ConstantTexts {
-  static const String versionInfo = "1.0.0 alpha";
-  static const int versionCheck = 100;
+  static const String versionInfo = "1.1.0 alpha";
+  static const int versionCheck = 101;
 }
